dcabe3fa290207bf871be1d93692efef74b3430a
jvignesh <jvignesh609@gmail.com>
2023-03-20 19:44:33 +0530

    Version 0.2

--- index.sh
+++ index.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 while [ true ]; do
-	echo "The Quiz Game"
-	echo "1.Sign In"
-	echo "2.Sign Up"
-	echo "3.Exit"
+	#menuBlock
+	clear
+	width=$COLUMNS
+	printf "\033[33m"
+	string="The Quiz Game"
+	printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+	sleep 0.2
+	string="1.Login"
+	printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+	sleep 0.3
+	string="2.Sign Up"
+	printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+	sleep 0.2
+	string="3.Exit"
+	printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+	sleep 0.3
+	printf "\033[0m"
 	echo "Enter Your Choice: "
 	read choice
-	#Main Block
+	#Creation Block
 	if [ ! -d user ];then
 		mkdir user
-		mkdir user/Answers
 	fi
-	case $choice in
-		1 )
-			echo "Welcome to Sign In:"
-			read -p "Enter Your User Name: " userName
-			if [ -e user/$userName.txt ]
-			then
+	#function Block 
+	function login(){
+		sleep 0.5
+		clear
+		printf "\033[33m"
+		string="--------------- Welcome to Sign In ---------------"
+		printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+		printf "\033[0m"
+		read -p "Enter Your User Name: " userName
+		if [ -e user/$userName.txt ]
+		then
+			chkUserName=`grep "User Name: $userName " user/$userName.txt `
+			if [[ $chkUserName == "User Name: $userName " ]]; then
 				read -sp "Enter Your password: " password
 				echo $password
 				chkPassword=`grep "password: $password" user/$userName.txt `
@@ -27,28 +46,66 @@ while [ true ]; do
 					. ./quiz.sh
 					bash quiz.sh 
 				else
-					echo "Wrong password!"
+					echo " "
+					printf "\033[91mWrong password!\033[0m"
+					sleep 2
+					login
 				fi
 			else
-			  	echo "Kindly Sign Up !"
-			fi;;
-		2 )
-			echo "Welcome to Sign Up:"
+				echo "  "
+				printf "\033[91mSomething Wrong! Kindly Check your user name\033[0m"
+				sleep 2
+			fi
+		else
+		  	echo ""
+		  	printf "\033[91mUser Not Exists! Try Again.\033[0m"
+		  	sleep 2
+		  	echo " "
+		  	read -p "1.Try Again | 2.Signup :" logch
+		  	if [[ $logch = 1 ]]; then
+		  		login
+		  	elif [[ $logch = 2 ]];then
+		  		  SignUp
+		    else
+		    	echo "Default SignUp!"
+		    	SignUp
+		  	fi
+		fi
+	}
+	function SignUp(){
+			sleep 0.5
+			clear
+			printf "\033[33m"
+			string="--------------- Welcome to Sign Up ---------------"
+			printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+			sleep 0.3
+			printf "\033[0m"
 			read -p "Enter Your User Name: " userName
 			if [ -e user/$userName.txt ]
 			then
-				echo "User Already Exists!"
+				printf "\033[91mUser Already Exists! Try Again.\033[0m"
+				sleep 2
+				SignUp
 			else
 				echo "User Name: $userName " >> user/$userName.txt
 				read -sp "Enter the password: " password
 				echo "password: $password" >> user/$userName.txt
 				echo $password
-				echo "Sign Up Completed ! "
-			fi;;
+				printf "\033[92mSign Up Completed! Login to play the quiz.\033[0m "
+				sleep 2
+			fi
+	}
+	#mainBlock
+	case $choice in
+		1 )
+			login;;
+		2 )
+			SignUp;;
 		3 )
-			echo "Exiting....."
+			echo "Exit Successfully." 
 			exit 0;;
 		* )
-			echo "Wrong Input!"	
+			printf "\033[91mWrong Input!\033[0m"
+			sleep 0.7
 	esac
 done
--- quiz.sh
+++ quiz.sh
@@ -1,66 +1,165 @@
 while [ true ];do
+		#Creation Block
+		width=$COLUMNS
 		if [ $userName ];then
 			welcomeName=$userName
 		else
 			welcomeName=$newUserName
 		fi
-		echo "---------- Welcome $welcomeName ----------"
-		echo "1.Play Game"
-		echo "2.Change User Name"
-		echo "3.Delect Account"
-		echo "4.Logout"
-		read gamechoice
-		case $gamechoice in 
-			1 )
-				echo "---------- Welcome To the Game ----------"
-				if [ -e quiz/quiz.txt ]
+		#functionBlock
+		function quizMenu(){
+				clear
+				printf "\033[33m"
+				string="--------------- Welcome To the Quiz Game ---------------"
+				printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+				string="Rules:"
+				printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+				string="1) Only Enter the A B C D options"
+				printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+				string="All the best !"
+				printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+				sleep 0.7	
+				printf "\033[0m"
+		}
+		function playQuiz(){
+				if [ -e quiz/1.txt ]
 				then
 					a=1
 					score=0
-					while [ $a -lt 4 ];do 
-						head -n 5 quiz/$a.txt
-						echo " "
-						read -p "Enter the answer: " answer
-						echo "$a) $answer" >> user/Answers/"$userName Answer.txt"
-						chkAns=`grep "$a) $answer" quiz/orgAns.txt`
-						if [[ $chkAns == "$a) $answer" ]];then
-							score=`expr $score + 1`
-						else
-							score=`expr $score + 0`
+					while [ -e quiz/$a.txt ];do
+						quizMenu 
+						if [ $a = 1 ];then
+							for (( i=3; i>0; i--)); do
+	  							sleep 1 &
+	  							printf "Starts in $i \r"
+	  							wait
+							done
 						fi
-						a=`expr $a + 1`
+						cat quiz/$a.txt
 						echo " "
+						sleep 0.5
+						read -p "Enter the answer: " answer
+						if [ $answer = "A" -o $answer = "B" -o $answer = "C" -o $answer = "D" ];then
+							echo "Correct Answer :" `sed "$a!d" quiz/orgAns.txt`     
+							# echo "Your Answer $a) $answer"
+							chkAns=`grep "$a) $answer" quiz/orgAns.txt`
+							if [[ $chkAns == "$a) $answer" ]];then
+								printf "\033[92mYour Answer $a) $answer"
+								score=`expr $score + 1`
+								printf "\n 👌 Correct! Nice.\033[0m"
+								sleep 2
+							else
+								printf "\033[91mYour Answer $a) $answer"
+								score=`expr $score + 0`
+								printf "\n 😅 Incorrect! Sorry!\033[0m"
+								sleep 2
+							fi
+							a=`expr $a + 1`
+							sleep 1
+						else
+							echo "Use A,B,C,D Choice only."
+							sleep 2
+						fi
 					done
-					echo "Your Score is : " $score
-					echo " "
-				fi;;
-			2 )
-				echo "---------- Change User Name ----------"
+					printf "\n\nYour Score is : $score/5"
+					sleep 3
+				fi
+		}
+		function changeUserName(){
+				clear
+				sleep 0.2
+				printf "\033[33m"
+				string="--------------- Change User Name ---------------"
+				printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+				printf "\033[0m"
 				read -p "Enter the new user name: " newUserName
-				if [ -e user/$userName.txt ];then
+				if [ ! -e user/$newUserName.txt ];then
 					mv -i user/$userName.txt user/$newUserName.txt
 					echo "User Name: $newUserName " > user/$newUserName.txt
 					echo "password: $password" >> user/$newUserName.txt
 					echo "User name changed Sucessfully."
-					bash index.sh
+					sleep 0.5
+					login
 					exit 1
 				else
-					echo "Error: Kindly Check! user exist."
-				fi;;
-			3 )
-				echo "---------- Delect Account ----------"
+					printf "\033[91mError: Kindly Check! user exist.\033[0m"
+					sleep 1
+					changeUserName
+				fi
+		}
+		function deleteAccount(){
+				clear
+				sleep 0.2
+				printf "\033[33m"
+				string="--------------- Delect Account ---------------"
+				printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+				printf "\033[0m"
 				if [ -e user/$userName.txt ];then
 					rm user/$userName.txt
-					echo "Account Delected." 
+					string="Account Delected."
+					printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+					sleep 2 
+					clear
 					bash index.sh
 					exit 0
 				else
 					echo "Error: Kindly Check! user name."
-				fi;;
+					deleteAccount
+				fi
+		}
+		function deleteConform(){
+				printf "\033[91m"
+				wrong="If you delete your account, Your data will be erased!"
+				printf "%$(((width-${#wrong})/2))s%s\n" "" "$wrong"
+				printf "\033[0m"
+				echo " "
+				read -p "Enter Your Choice: y/n " delch
+				if [ $delch = "y" ];then
+					deleteAccount
+				elif [ $delch = "n" ];then
+					echo "You try to delete your account! Kindly sign in to play the quiz."
+					sleep 0.8
+					login
+				else
+					printf "\033[91mWrong Input! Try Again.\033[0m"
+					sleep 0.8
+					deleteConform
+				fi
+		}
+		#menuBlock
+		clear
+		printf "\033[33m"
+		string="--------------- Welcome $welcomeName ---------------"
+		printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+		sleep 0.3
+		string="1.Play Game"
+		printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+		sleep 0.2
+		string="2.Change User Name"
+		printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+		sleep 0.3
+		string="3.Delect Account"
+		printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+		sleep 0.2
+		string="4.Logout"
+		printf "%$(((width-${#string})/2))s%s\n" "" "$string"
+		printf "\033[33m"
+		echo "Enter Your Choice: "
+		read gamechoice
+		case $gamechoice in 
+			1 )
+				playQuiz;;
+			2 )
+				changeUserName;;
+			3 )
+				deleteConform;;
 			4 ) 
+				clear
+				sleep 0.3
 				bash index.sh
 				exit 1;;
 			* )
 				echo "Error: Kindly Check Your choice."
+				sleep 1
 		esac
 done
